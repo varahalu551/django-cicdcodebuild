df9eeb5f1ac5e27a61d3b453a01501529662110e
varahalu51 <nagam.svb@gmail.com>
2024-10-23 17:33:04 +0530

    add message nginx.conf

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/Your_project_folder_name/mysite/settings.py
+#sed -i 's/\[]/\["Your_Instance_Public_IP"]/' /home/ubuntu/Your_project_folder_name/mysite/settings.py
 
-python manage.py migrate 
+#python manage.py migrate 
 # python manage.py makemigrations     
-python manage.py collectstatic
-sudo service gunicorn restart
-sudo service nginx restart
+#python manage.py collectstatic
+#sudo service gunicorn restart
+#sudo service nginx restart
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
@@ -19,3 +19,11 @@ sudo service nginx restart
 # Restart:
 #systemctl restart gunicorn
 #sudo systemctl status nginx
+
+sed -i 's/\[]/\["13.201.78.251"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+
+python manage.py migrate 
+python manage.py makemigrations     
+python manage.py collectstatic
+sudo service gunicorn restart
+sudo service nginx restart
