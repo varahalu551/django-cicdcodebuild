f45cc03a01bebf9fb2ac939db75630d2dd40e234
varahalu51 <nagam.svb@gmail.com>
2024-10-23 17:47:40 +0530

    install requirement and set up env

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -4,7 +4,7 @@
 #virtualenv /home/ubuntu/Project_folder_name/venv
 #source /home/ubuntu/Project_folder_name/venv/bin/activate
 #pip install -r /home/ubuntu/Project_folder_name/requirements.txt
-
+#change commands sudo apt-get install python3-venv,
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
